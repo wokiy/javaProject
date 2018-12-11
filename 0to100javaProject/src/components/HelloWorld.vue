918f39b7f8fc47f741199567280e971f360ef0c2
DESKTOP-4D9484E\jason <jasonlwy@163.com>
2018-12-11 21:20:22 +0800

    匠心坊开动81021211

---
+++ 0to100javaProject/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1 style="color: red;">{{ msg }}</h1>
+    <h2 style="color: seagreen;">linpaopao-java之旅</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your 匠心坊'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
